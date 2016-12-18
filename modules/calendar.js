5b4880afbcd80498d8014e200ad7703cbd37413a
Alex Garcia <alexsebastian.garcia@gmail.com>
2016-12-17 16:17:22 -0800

    Testing new way for gcal api

---
+++ modules/calendar.js
@@ -0,0 +1,23 @@
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+
+function landing(req, res) {
+
+	if(!req.query.courses) {
+		console.log('oh no');
+		return;
+	}
+
+	console.log(req.query);
+	var courseIds = req.query.courses.split('.');
+	
+	//res.send('ayyy still working on this mate');
+	res.render('test.html', {courses:courseIds, clientID: process.env.GCAL_CLIENTID});
+	
+}
+
+module.exports = {
+	landing: landing,
+	
+}
