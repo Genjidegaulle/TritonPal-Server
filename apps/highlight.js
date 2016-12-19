61ee8db3e2f9ecae25475d350208c6b16b53dc00
Alex Garcia <alexsebastian.garcia@gmail.com>
2016-12-18 23:57:46 -0800

    trying on mustache on highlight app

--- apps/highlight.js
+++ apps/highlight.js
@@ -4,46 +4,40 @@ var redis = require('../modules/redis.js');
 
 function landing(req, res) {
 
-	var subjCode = req.query.subj_code;
-	var courseCode = req.query.course_code;
 	var classCode = req.query.class_code;
+	var type = req.query.type;
 
 
 	if(classCode && classCode.length > 0) {
-
+		
+		//clean up commas and shit
 		classCode = classCode.replace(/[^a-zA-Z0-9]/g, '');
-		redis.incCourse(classCode);
+
 
 		ucsd_courses.getCourseData(classCode, function(err, courseObj) {
+
 				if(err) {
 					console.log(err);
 					res.send(JSON.stringify(err));
 				}
-				else {
-					res.send(JSON.stringify(courseObj));
+				else if(courseObj.err) {
+					console.log(courseObj.err);
+					res.send(JSON.stringift(err));
 				}
+				else {
 
+					redis.incCourse(classCode);
+
+					if(type == 'json') {
+						res.send(JSON.stringify(courseObj));
+					}
+					else {
+						res.render('popup-blank.html', courseObj);
+					}
+				}
 		});
-	}
-  else if(typeof subjCode === 'undefined' || subjCode === null || subjCode.length < 1||
-		    typeof courseCode === 'undefined' || courseCode === null || courseCode.length < 1) {
 
-		    console.log('ruh roh');
-					res.send(JSON.stringify({err: {message: 'subj_code and course_code both must be defined for this api'}}));
-					return;
-	}
 
-	else {
-
-		ucsd_courses.getCourseData(subjCode+' '+courseCode, function(err, courseObj) {
-			if(err) {
-				console.log(err);
-			}
-			else {
-				res.send(JSON.stringify(courseObj,null,'\t'));
-			}
-
-		});
 	}
 }
 
