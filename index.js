b075496b350fc31fde18ddbb931ec9fa57e20b27
Alex Garcia <alexsebastian.garcia@gmail.com>
2016-12-18 23:16:14 -0800

    made structure different in index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var express = require('express');
 var ucsd_courses = require('./modules/ucsd_courses.js');
 var redis = require('./modules/redis.js');
 var mustacheExpress = require('mustache-express');
-var calendarHandler = require('./modules/calendar.js');
+
+var calendarApp = require('./apps/calendar.js');
+var prettyApp = require('./apps/pretty.js');
+var highlightApp = require('./apps/highlight.js');
 
 
 var app = express();
@@ -29,68 +32,21 @@ app.get('/', function(request, response) {
 //#########   APPS    ###############
 
 //Calendar App
-
-
-app.get('/apps/calendar/landing.html', calendarHandler.landing);
-
+app.get('/apps/calendar/landing.html', calendarApp.landing);
 
 //Pretty App
-app.get('/apps/pretty/test.css', function(req, res) {
-	res.sendFile(__dirname + '/test.css');
-});
+app.get('/apps/pretty/test.css', prettyApp.landing);
 
+//app.get('/apps/highlight', highlightApp.landing);
 
 //Course highlight app
-app.get('/apps/courses', function(req, res) {
-	
+app.get('/apps/courses', highlightApp.landing);
 
-	
-	var subjCode = req.query.subj_code;	
-	var courseCode = req.query.course_code;
-	var classCode = req.query.class_code;
 
-	
-	if(classCode && classCode.length > 0) {
+//DAVID PUT YOUR STUFF HERE
 
-		classCode = classCode.replace(/[^a-zA-Z0-9]/g, '');
-		redis.incCourse(classCode);
 
-		ucsd_courses.getCourseData(classCode, function(err, courseObj) {
-			if(err) {
-				console.log(err);
-				res.send(JSON.stringify(err));
-			}
-			else {
-				res.send(JSON.stringify(courseObj));
-			}
 
-		});
-	}
-
-	else if(typeof subjCode === 'undefined' || subjCode === null || subjCode.length < 1||
-		typeof courseCode === 'undefined' || courseCode === null || courseCode.length < 1) {
-		
-		console.log('ruh roh');
-		res.send(JSON.stringify( 
-			{err: {message: 'subj_code and course_code both must be defined for this api'}}));
-		return;
-	}
-
-  else {
-		
-		ucsd_courses.getCourseData(subjCode+' '+courseCode, function(err, courseObj) {
-			if(err) {
-				console.log(err);
-			}
-			else {
-				res.send(JSON.stringify(courseObj,null,'\t'));
-			}
-
-		});
-	}
-  
-
-});
 
 
 
