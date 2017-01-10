efeecf4b2d300c310a203b8e40f2a7ef51891f54
David Liau <dliau@ucsd.edu>
2017-01-09 16:29:36 -0800

    doesn't work fml

--- public/apps/calendar/landing.js
+++ public/apps/calendar/landing.js
@@ -34,7 +34,7 @@ if (!response) {
 
 response.onload = function() {
  scraped_courses = JSON.parse(response.responseText);
- alert(scraped_courses);
+ console.log(scraped_courses);
  // process the response.
 };
 
