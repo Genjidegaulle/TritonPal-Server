8df0db0f7f59c9c1ac89805c5339d90741280a25
Alex Garcia <alexsebastian.garcia@gmail.com>
2017-01-23 23:35:22 -0800

    lol plz start working again

--- public/apps/calendar/landing.js
+++ public/apps/calendar/landing.js
@@ -104,62 +104,6 @@ function loadCalendarApi() {
 	gapi.client.load('calendar', 'v3', listCalendars);
 }
 
-function listCalendars() {
-	console.log('before req');
-	var request = gapi.client.calendar.calendarList.list();
-	console.log('before exec');
-	request.execute(function(resp) {
-			appendPre('results:\n');
-			for(var i = 0; i < resp.items.length; i++)
-			 appendPre(resp.items[i].summary + '\n');
-			});
-}
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-	var request = gapi.client.calendar.events.list({
-			'calendarId': 'primary',
-			'timeMin': (new Date()).toISOString(),
-			'showDeleted': false,
-			'singleEvents': true,
-			'maxResults': 10,
-			'orderBy': 'startTime'
-			});
-
-	request.execute(function(resp) {
-			var events = resp.items;
-			appendPre('Upcoming events:');
-
-			if (events.length > 0) {
-			for (i = 0; i < events.length; i++) {
-			var event = events[i];
-			var when = event.start.dateTime;
-			if (!when) {
-			when = event.start.date;
-			}
-			appendPre(event.summary + ' (' + when + ')')
-				}
-				} else {
-				appendPre('No upcoming events found.');
-				}
-
-				});
-			}
-
-			/**
-			 * Append a pre element to the body containing the given message
-			 * as its text node.
-			 *
-			 * @param {string} message Text to be placed in pre element.
-			 */
-function appendPre(message) {
-	var pre = document.getElementById('output');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
-}
 
 function showClasses(){
   var classes = [];
@@ -287,8 +231,9 @@ function addEvent() {
   				});
 
   		request.execute(function(exevent) {
-  				appendPre('Event created: ' + event.htmlLink);
-  				});
+
+				alert('wooo done');
+			});
   	}
   }
 }
